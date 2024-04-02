e69f804784ab628e463e7d4e65c5525b0484c254
unknown <moshat.medhashree15@gmail.com>
2024-04-02 20:02:19 +0530

    Implemented the game logic

--- src/App.js
+++ src/App.js
@@ -1,18 +1,75 @@
 import Player from "./components/Player";
 import GameBoard from "./components/GameBoard";
+import { WINNING_COMBINATIONS } from "./components/WinningCombinations";
+import GameOver from "./components/GameOver";
+import { useState } from "react";
+
+function derivedActivePlayer(gameTurns){
+  let currentPlayer = 'X';
+
+      if(gameTurns.length > 0 && gameTurns[0].player === 'X'){
+        currentPlayer = 'O';
+      }
+  return currentPlayer;
+}
+
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+
+let winner;
+let gameBoard = [...initialGameBoard.map(array => [...array])];
 
 function App() {
+  const [gameTurns, setGameTurns] = useState([]);
+
+  const activePlayer = derivedActivePlayer(gameTurns);
+
+
+  for(const turn of gameTurns){
+    const {square, player} = turn;
+    const {row, col} = square;
+
+    gameBoard[row][col] = player;
+  }
+
+  for(const combination of WINNING_COMBINATIONS){
+    const firstSquareSymbol = gameBoard[combination[0].row][combination[0].column];
+    const secondSquareSymbol = gameBoard[combination[1].row][combination[1].column];
+    const thirdSquareSymbol = gameBoard[combination[2].row][combination[2].column];
   
+    if(firstSquareSymbol && firstSquareSymbol === secondSquareSymbol && firstSquareSymbol === thirdSquareSymbol){
+      winner = firstSquareSymbol;
+    }
+  }
+
+  const hasDraw = gameTurns.length === 9 && !winner;
+
+  function handleSelectSquare(rowIndex, colIndex){
+    setGameTurns(prevTurns => {
+      const currentPlayer = derivedActivePlayer(prevTurns);
+
+      const updatedTurns = [{square: {row: rowIndex, col: colIndex}, player: currentPlayer}, ...prevTurns];
+      return updatedTurns;
+    })
+  }
+
+  function handleRestart(){
+    setGameTurns();
+  }
 
   return (
     <main>
       <h1>React Tic-Tac-Toe</h1>
       <div id="game-container">
-        <ol id="players">
-          <Player defaultName="Player 1" symbol="X" />
-          <Player defaultName="Player 2" symbol="O" />
+        <ol id="players" className="highlight-player">
+          <Player defaultName="Player 1" symbol="X" isActive={activePlayer === 'X'}/>
+          <Player defaultName="Player 2" symbol="O" isActive={activePlayer === 'O'}/>
         </ol>
-        <GameBoard />
+        {(winner || hasDraw) && <GameOver winner={winner} onRestart={handleRestart} />}
+        <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard}/>
       </div>
     </main>
   )
--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -1,18 +1,25 @@
-const initialGameBoard = [
-  [null, null, null],
-  [null, null, null],
-  [null, null, null],
-];
+export default function GameBoard({onSelectSquare, board}) {
+  // const [gameBoard, setGameBoard] = useState(initialGameBoard);
 
-export default function GameBoard() {
+  // function handleSquareClick(rowIndex, colIndex) {
+  //   setGameBoard((prevGameBoard) => {
+  //     const updatedGameBoard = [
+  //       ...prevGameBoard.map(innerArray => [...innerArray])
+  //     ];
+  //     updatedGameBoard[rowIndex][colIndex] = activePlayerSymbol;
+  //     return updatedGameBoard;
+  //   });
+  //   onSelectSquare();
+  // }
+
   return (
     <ol id="game-board">
-      {initialGameBoard.map((row, rowIndex) => (
+      {board.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button>{playerSymbol}</button>
+                <button onClick={() => onSelectSquare(rowIndex, colIndex)} disabled = {playerSymbol !== null}>{playerSymbol}</button>
               </li>
             ))}
           </ol>
